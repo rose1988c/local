240ee8377cfa300d105e947ae6af6d0f2c440bab
rose1988.c@gmail.com <rose1988.c@gmail.com>
2014-01-23 15:35:35 +0800

    update

--- test.php
+++ test.php
@@ -1,4 +1,47 @@
 <?PHP
+
+    require_once ('service_center/lib/LocalServiceCenter.class.php');
+    
+    
+    $data = (array) LocalServiceCenter::instance('Prop.Info.Get')
+    ->args(array(
+        'areaid' => 1088,
+        'numid' => 11625804,
+        'propids' => 50,
+    ))
+    ->get_data();
+    
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+    die();
+    
+    
+    $data = (array) LocalServiceCenter::instance('Match.Sub.Get')
+    ->args(array(
+        'type' => 'NEWPT',
+        'matchid' => 110,
+    ))
+    ->get_data();
+    
+    echo '<pre>';
+    print_r($data);
+    
+    
+    $data = (array) LocalServiceCenter::instance('Match.Rank.Get')
+    ->args(array(
+            'type' => 'NEWPT',
+            'matchid' => 110,
+            'subid' => 1388361600,
+    ))
+    ->get_data();
+    
+    echo '<pre>';
+    print_r($data);
+    echo '<pre>';
+    die();
+    
+
     $op_left = '123456789123456789';
     $op_right = '123456789123456789';
     echo(mt_rand($op_right,$op_left));
